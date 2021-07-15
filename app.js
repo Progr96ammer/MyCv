e8726b55576d22b5f2af0e4341fe1e7584c73437
Progr96ammer <69406656+Progr96ammer@users.noreply.github.com>
2021-07-16 01:13:58 +0300

    do

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const port = process.env.port || '3000';
+const port = process.env.port || '3001';
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
